fb26f903506ba9156bbc8df760793c9063faee4b
Gabriel Aguiar <lugafeagre@gmail.com>
2024-12-28 19:37:58 -0300

    refactor(pages): :truck: rename search.tsx to filter.tsx

---
+++ app/filter.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Surface, Text } from 'react-native-paper'
+import { styles } from '@/lib/ui'
+
+const Filter = () => {
+  return (
+    <Surface style={styles.screen}>
+      <Text>Hello Filtros</Text>
+    </Surface>
+  )
+}
+
+export default Filter
